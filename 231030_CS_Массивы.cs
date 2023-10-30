ce4958602662664fecead6ab8b0a80cd7cc97fae
Klimov_Maxim <120740542+MakSimKlim@users.noreply.github.com>
2023-10-30 21:34:33 +0500

    Add files via upload

---
+++ 231030_CS_Массивы.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+namespace _231030_CS_Массивы
+{
+    class Program
+    {
+        static void Arrays()
+        {
+            //=============== одномерные массивы====================
+
+            // int ar[100]; - запись массива в С++
+            // все типы массивов унаследованы от System.Array
+            int[] ar = new int[50]; // синтаксис записи массива в С#, отличается от С++
+            // new - объявляются экземпляры классов
+            string[] s = new string[5]; // - они все сейчас по-умолчанию null (пустое место)
+
+            // в отл от С++ не можем указать больше или меньше данных
+            // указываем их ровное кол-во, 
+            int[] ar2 = new int[5] { 1, 2, 3, 4, 5 };
+            //либо не указываем вообще
+            int[] ar3 = new int[] { 1, 2, 3, 4, 5, 6 };
+            // либо сразу списком инициализации
+            int[] ar4 = { 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+            // нельзя так :
+            //int[] ar5;
+            //ar5 = { 1,2,3,4,5};
+            // можно так:
+            int[] ar5;
+            ar5 = new int[] { 1, 2, 3, 4, 5 };
+            // заменить элементы можно так:
+            Console.WriteLine(ar2[2]);
+            ar5[3] = 99;
+            Console.WriteLine(ar5[3]);
+
+            //=============== многомерные массивы====================
+            // ! всегда прямоугольные
+            float[,] m = new float[2, 3];
+            int[,] m1 = new int[2, 3] { { 1, 2, 3 }, { 4, 5, 6 } };
+            int[,] m2 = new int[,] { { 1, 2 }, { 3, 4 }, { 5, 6 } };
+            int[,] m3 = { { 1, 2, 3, 4 }, { 5, 6, 7, 8 } };
+            int[,,] m3D = new int[3, 4, 2];//трехмерный многомерный массив
+            Console.WriteLine(m1[1, 1]);// вывод значений
+
+            // зубчатые массивы (не всегда прямоугольные):
+            int[][] jagged = new int[5][];
+            jagged[0] = new int[] { 1, 2 };
+            jagged[1] = new int[] { 3, 4, 5 };
+
+            // атрибуты(методы) массивов
+            Console.WriteLine(ar2.Length); // длина массива
+            Console.WriteLine(ar2.GetLowerBound(0) + ar2.GetUpperBound(0));//возвращает нижний и наибольший индекс
+            // копирование в массив CopyTo
+            /*int[] arn = new int[5];
+            ar2.CopyTo(arn);*/
+            // копия CopyTo()
+            // метод Clear:
+            Array.Clear(ar2, 0, 4);// с указанного индекса приравнивает их к нулю
+            Array.IndexOf(ar2, 4);// возврвщает индекс в массиве (первое вхождение)
+            Array.LastIndexOf(ar2, 1);// возврвщает индекс в массиве (последнее вхождение)
+            Array.Resize<int>(ref ar2, 6); // изменяет размер массива
+            Array.Reverse(ar2); // разворот массива
+            Array.Sort(ar2); // сортировка массива
+
+            Console.WriteLine(ar2.Rank); // возвращает мерность массива
+            Console.WriteLine(m3D.Rank); // возвращает мерность массива
+            Console.WriteLine(jagged.Rank); // возвращает мерность массива (у зубчатого всегда ранг 1, потому что это массив массивов, а не многомерный массив)
+
+        }
+        static void Main(string[] args)
+        {
+            int[,] m = new int[,] { { 1, 2 }, { 3, 4 }, { 5, 6 } };
+            foreach (int e in m)
+            {
+                Console.WriteLine(e);
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                    Console.Write(m[i, j].ToString() + '\t');
+                Console.WriteLine();
+            }
+
+            int[][] jagged = new int[3][];
+            jagged[0] = new int[] { 1, 2 };
+            jagged[1] = new int[] { 3, 4, 5 };
+            jagged[2] = new int[] { 6,7,8,9 };
+
+            for (int i = 0; i < jagged.Length; i++)
+            {
+                for (int j = 0; j < jagged.Length; j++)
+                    Console.Write(jagged[i][j].ToString() + '\t');
+                Console.WriteLine();
+            }    
+        }
+    }
+}
---
+++ 231030_CS_продолжение_теория.cs
@@ -0,0 +1,165 @@
+﻿using System;
+
+namespace _231030_CS_продолжение_теория
+{
+    class Program
+    {
+        static void BasicConvert()
+        {
+            // явное преобразование
+            int x = 5;
+            float y = 6.5f;
+            float b = y + x;// это допустимо
+            // int a = y+x; // это недопустимо
+            int a = (int)(y + x);
+        }
+        static void AdvancedConvert()
+        {
+            // если преобразования невозможны, можно использовать
+            // конвертацию через статические методы класса Convert
+            Console.Write("Ведите целое число: ");
+            string numberString = Console.ReadLine();
+            int number = Convert.ToInt32(numberString);
+            Console.Write("Строка конвертирована, значение: " + number);
+            // Также есть статический метод Parse  у классов типов данных
+            // Но он работает только со строками.
+            number = Int32.Parse(numberString);
+            Console.WriteLine();
+            //  у всех встроенных классов есть метод ToString,
+            // который возвращает значение, преобразованное в строку
+            number.ToString();
+        }
+
+        static public int a = 10;
+
+        static bool CheckEvenAndIncreaseA(int b)
+        {
+            a++;
+            return b % 2 == 0;
+        }
+
+        static void LogicOperators()
+        {
+            // разница в написании && и &
+            int b = 5;
+            if (b == 6 & CheckEvenAndIncreaseA(b)) // & проверяет оба условия, даже если слева ложь
+            {
+                Console.WriteLine("b was even ");
+            }
+            else
+            {
+                Console.WriteLine("b wasn't even or 6");
+            }
+            Console.WriteLine(a);
+
+            int c = 7;
+            if (c == 6 && CheckEvenAndIncreaseA(c))// && проверит только слева, если справа проверять уже не будет
+            {
+                Console.WriteLine("c was even ");
+            }
+            else
+            {
+                Console.WriteLine("c wasn't even or 6");
+            }
+            Console.WriteLine(a);
+
+        }
+
+        static void ByteOperators()
+        {
+            // побитовые операции
+            int a = 5 & 6; // если слева и справа числа - становится побитовым оператором
+            int b = 5 | 6; // если слева и справа числа - становится побитовым оператором
+
+            int and = 5 & 6;     // если слева и справа числа - становится побитовым оператором
+            int or = 7 | 11;     // если слева и справа числа - становится побитовым оператором
+            int xor = 10 ^ 15;   // побитовое исключающее ИЛИ
+            int shiftR = 10 >> 1; // побитовый сдвиг
+            int shiftL = 10 << 1;// побитовый сдвиг
+            int not = ~6;
+            Console.WriteLine(and.ToString() + ' ' + or.ToString() + ' ' + xor.ToString());
+            // приоритет у двойных ниже чем у одинарных
+
+        }
+
+        static void Switch()
+        {
+            //switch case
+            int a = 5;
+            switch (a)
+            {
+                case 1:
+                    break;
+                case 2: // пустой блок, толькк при пустом блоке может НЕ стоять dreak
+                        //Console.WriteLine("2 or 3")// так нельзя если нет Break!
+                case 3:
+                    Console.WriteLine("2 or 3");
+                    break;
+                default:
+                    break;
+
+            }
+
+        }
+
+        static void Cicles()
+        {
+            // циклы такиее же как в С++, но есть еще циклы по коллекции
+            // for
+            // while do... while
+            // foreach
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            for (int x = 0, y = 0; x < 10 && y < 10; x++, y++)
+            {
+                Console.WriteLine(x.ToString() + ' ' + y.ToString());
+            }
+
+            Random rand = new Random(); // генерирует случайные числа
+            while (true) // только значение bool (цифр не должно быть в отличие от С++)
+            {
+                int a = rand.Next();
+                Console.WriteLine(a);
+                if (a % 3 == 0)
+                    break;
+            }
+
+            int k = 0;
+            do
+            {
+                int a = 10;
+                Console.WriteLine(a * k);
+                k++;
+            } while (a % 2 != 0);
+
+            // цикл foreach() - цикл коллекций 
+            // этот цикл только для чтения? его можно перезаписать только строкой
+
+            int j = 3;
+        Label1: // метка
+            string s = "Hello world";
+            foreach (char с in s)
+            {
+                //с = '1';
+                Console.WriteLine(c);
+                if (с == 'o' && j-- > 0)
+                    goto Label1;// лучше этим не пользоваться, т.к. не понятно что происходит с данными в памяти
+                // goto делает код непредсказуемым, большие угрозы утечки памяти
+                // также неудобно следить за ним в коде
+                // в очень узком применении его можно использовать
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+           
+
+        }
+
+
+    }
+}
