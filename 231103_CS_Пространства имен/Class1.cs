95072413ca26dfd6c5f014ec8ef99c275e43e6bd
Klimov_Maxim <120740542+MakSimKlim@users.noreply.github.com>
2023-11-03 21:41:04 +0500

    Add files via upload

---
+++ 231103_CS_Пространства имен/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NS1
+
+{
+    class Class1
+    {
+        public Class1()
+        {
+            Console.WriteLine("создан объект класса2");
+        }
+
+    }
+}
---
+++ 231103_CS_Пространства имен/Class2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using NS1;
+
+namespace NS2
+{
+        class Class2
+        {
+            public Class2()
+            {
+                Console.WriteLine("создан объект класса2");
+                Class1 c1 = new Class1();
+            }
+
+        }
+    
+}
