ef3b6b2c0b006da1154c67b3b9239aafb17457bf
Klimov_Maxim <120740542+MakSimKlim@users.noreply.github.com>
2023-11-17 21:20:26 +0500

    Add files via upload

---
+++ 231117_CS_генерики/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _231117_CS_генерики
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
